69d5cfd733f9fa92fcd0e5e3e2fb3bba6fc134a0
Pavel-Romanov99 <45098190+Pavel-Romanov99@users.noreply.github.com>
2019-03-07 17:44:18 +0200

    Exercises 1: Point Class
    Exercises 1: Point Class

---
+++ Classes/point.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class Point
+{
+private:
+	int x;
+	int y;
+
+public:
+	void set(int new_x, int new_y)
+	{
+		x = new_x;
+		y = new_y;
+	}
+
+	int get_x()
+	{
+		return x;
+	}
+
+	int get_y()
+	{
+		return y;
+	}
+
+	void change_x(int a)
+	{
+		x += a;
+	}
+
+	void change_y(int b)
+	{
+		y += b;
+	}
+};
+
+
+int main()
+{
+	Point a;
+	a.set(1, 3);
+	cout << a.get_x() << " " <<  a.get_y() << endl;
+
+	a.change_x(2);
+	a.change_y(3);
+	cout << a.get_x() << " " << a.get_y() << endl;
+
+}
