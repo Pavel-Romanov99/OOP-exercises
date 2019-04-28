7a3eee3d24cd970fa9c70debc84b9ec7c94be8dd
Pavel-Romanov99 <45098190+Pavel-Romanov99@users.noreply.github.com>
2019-04-28 13:56:11 +0300

    Song playlist
    Song playlist

--- Classes/Source.cpp
+++ Classes/Source.cpp
@@ -1,219 +1,144 @@
 #include <iostream>
-#include <cmath>
-#include <string>
+#include <cstring>
 using namespace std;
 
-const int MAX = 20;
-class BankAccount
-{
+int const Max = 100;
+class Song {
 private:
-	string name;
-	string id_number;
-	double balance;
+	char title[Max];
+	char artist[Max];
+	int duration;
 public:
-	BankAccount()
+
+	Song()
 	{
-		name = "";
-		id_number = "";
-		balance = 0.0;
+		strcpy_s(title, "");
+		strcpy_s(artist, "");
+		duration = 0;
 	}
-	BankAccount(string new_name, string new_id, double new_balance)
+
+	Song(char const * new_title, char const* new_artist, int new_duration)
 	{
-		name = new_name;
-		id_number = new_id;
-		balance = new_balance;
+		strncpy_s(this->title, new_title, Max - 1);
+		this->title[Max - 1] = '/0';
+		strncpy_s(artist, new_artist, Max - 1);
+		this->artist[Max - 1] = '/0';
+		this->duration = new_duration;
 	}
-	void print()
+
+	char const* getTitle() const
 	{
-		cout << "Name: " << name << endl;
-		cout << "Id number: " << id_number << endl;
-		cout << "Balance: " << balance << endl;
-	} 
-	void add_money(int x)
-	{
-		balance += x;
+		return title;
 	}
-	void take_money(int x)
+
+	char const* getArtist() const
 	{
-		balance -= x;
+		return artist;
 	}
+
+	int getDuration()
+	{
+		return duration;
+	}
+
+	void printSong()
+	{
+		cout << getTitle() << " " << getArtist() << " " << getDuration() << endl;
+ 	}
 };
 
-class Factoriel
+class Playlist
 {
 private:
-	int a;
-public:
-	void set(int x)
+	Song* playlist;
+	int capacity;
+	int size;
+
+	void copy(Playlist const &other)
 	{
-		a = x;
-	}
-	int fact()
-	{
-		int fact = 1;
-		for (int i = 1; i <= a; i++)
+		size = other.size;
+		capacity = other.capacity;
+		playlist = new Song[other.capacity];
+		for (int i = 0; i < other.size; i++)
 		{
-			fact = fact * i;
+			playlist[i] = other.playlist[i];
 		}
-		return fact;
 	}
-};
-
-class Absolute
-{
-private:
-	int a;
-	double b;
 public:
-	void set_int(int x)
+	Playlist(int num)
 	{
-		a = x;
-	}
-	void set_real(double y)
-	{
-		b = y;
-	}
-	int absolute_int()
-	{
-		return abs(a);
-	}
-	double absolute_real()
-	{
-		return abs(b);
-	}
-};
-
-class Employee
-{
-	struct  {
-		string first_name;
-		string surname;
-	}name;
-	struct work {
-		int hours;
-		int salary;
-	}work;
-public:
-	void read()
-	{
-		cout << "Name: "; cin >> name.first_name;
-		cout << "Surname: "; cin >> name.surname;
-		cout << "Working hours: "; cin >> work.hours;
-		cout << "Salary: "; cin >> work.salary;
-	}
-	void print()
-	{
-		cout << name.first_name << " " << name.surname << endl; 
-		cout << "Money: " << work.hours* work.salary << endl;
-	}
-};
-
-const double GET_TO_RAD = 3.14 / 180;
-class Angle
-{
-private:
-	int angle;
-public:
-	void set_angle(int x)
-	{
-		angle = x;
-	}
-	double get_sin()
-	{
-		return sin(GET_TO_RAD * angle);
-	}
-	double get_cos()
-	{
-		return cos(GET_TO_RAD * angle);
-	}
-	double get_tan()
-	{
-		return tan(GET_TO_RAD * angle);
-	}
-};
-
-
-class C
-{
-private:
-	int a;
-	int b;
-public:
-	void init(int x, int y)
-	{
-		a = x;
-		b = y;
-	}
-	void display()
-	{
-		cout << a << " " << b << endl;
-	}
-};
-
-class Point3
-{
-private:
-	int x;
-	int y;
-	int z;
-public:
-	Point3()
-	{
-		x = 0;
-		y = 0;
-		z = 0;
+		capacity = num;
+		size = 0;
+		playlist = new Song[capacity];
 	}
 
-	Point3(int new_x, int new_y, int new_z)
-	{
-		x = new_x;
-		y = new_y;
-		z = new_z;
+	Playlist(Playlist const& other) {
+		copy(other);
 	}
 
-	Point3(const Point3& p)
-	{
-		x = p.x + 1;
-		y = p.y + 2;
-		z = p.z + 3;
+	~Playlist() {
+		delete[] playlist;
 	}
 
-	int get_x()
+	void addSong(Song const &song)
 	{
-		return x;
+		if (size < capacity)
+		{
+			playlist[++size] = song;
+		}
+		else cout << "Playlist is full! " << endl;
 	}
 
-	int get_y()
+	void printPlaylist()
 	{
-		return y;
+		for (int i = 1; i < size + 1; i++)
+		{
+			cout << playlist[i].getTitle() << " " << playlist[i].getArtist() << " " << playlist[i].getDuration() 
+				<< endl;
+		}
 	}
 
-	int get_z()
+	int totalDuration()
 	{
-		return z;
+		int sum = 0;
+		for (int i = 0; i < size; i++)
+		{
+			sum += playlist[i].getDuration();
+		}
+		return sum;
 	}
 
-	void new_coordinates(int new_x, int new_y, int new_z)
+	void longestSong()
 	{
-		x = new_x;
-		y = new_y;
-		z = new_z;
-	}
-	void print()
-	{
-		cout << x << " " << y << " " << z << endl;
+		Song longest_song;
+		for (int i = 0; i < size; i++)
+		{
+			if (longest_song.getDuration() < playlist[i].getDuration())
+			{
+				longest_song = playlist[i];
+			}
+		}
+		longest_song.printSong();
 	}
 };
 
 int main()
 {
-	Point3 point(1, 2, 3);
-	point.print();
-	point.new_coordinates(2, 3, 4);
-	point.print();
-	Point3 new_point;
-	new_point.print();
-	Point3 new_point1 = point;
-	new_point1.print();
-
-} 
+	Song a("MMA", "Azis", 323); 
+	Song b("Otlichen 6", "Toni Storaro", 420);
+	Song c("Poludei", "Djordan", 350);
+	Song d("Nonstop", "Drake", 212);
+	Song e("Bel djip", "BNR", 422);
+	Song f("Dim4ou", "422", 320);
+	Song g("Krisko", "Nezamenim", 293);
+	Playlist ab(50);
+	ab.addSong(a);
+	ab.addSong(b);
+	ab.addSong(c);
+	ab.addSong(d);
+	ab.addSong(e);
+	ab.addSong(f);
+	ab.addSong(g);
+	cout << ab.totalDuration() << endl;
+	ab.longestSong();
+}
